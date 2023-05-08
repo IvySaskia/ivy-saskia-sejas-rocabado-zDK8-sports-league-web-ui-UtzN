458245e484fc67460daec8a3af53337b4b2872bd
IvySaskia <ivysaskia@gmail.com>
2023-05-08 18:53:04 -0400

    ADD: Custom font

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -9,10 +11,11 @@ import Schedule from "./pages/schedule";
 import Leaderboard from "./pages/leaderboard";
 import NoPage from "./pages/no-page";
 
+import theme from './theme'
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <MainLayout>
         <Switch>
           <Route exact path="/">
